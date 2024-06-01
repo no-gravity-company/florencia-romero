69bf0635054ef7eef36b7feca5bdb8c57bbb0f54
Jack Blakeston <jackblakeston93@gmail.com>
2024-06-02 00:13:38 +0200

    Add env variable for firebase config

--- src/assets/js/carousel.js
+++ src/assets/js/carousel.js
@@ -1,15 +1,3 @@
-const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyAawADZXF389VEKEUzopuFhLxKLvoc_yhE',
-  authDomain: 'florencia-romero-web.firebaseapp.com',
-  projectId: 'florencia-romero-web',
-  storageBucket: 'florencia-romero-web.appspot.com',
-  messagingSenderId: '963362284322',
-  appId: '1:963362284322:web:afe661b5e0567cc6ed7b7b',
-  measurementId: 'G-K61ZN97RLW',
-};
-
-firebase.initializeApp(FIREBASE_CONFIG);
-
 const storage = firebase.storage();
 const storageRef = storage.ref();
 const carrouselImagesPath = 'carousel-images/';
